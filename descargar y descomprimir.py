42970f68fb62f1c6faa7e24aade5cf9d6a391dfc
TheWildBoss <129200265+TheWildBoss@users.noreply.github.com>
2024-05-13 23:44:18 +0000

    Mundo guardado 2024-05-13

--- descargar y descomprimir.py
+++ descargar y descomprimir.py
@@ -3,10 +3,10 @@ import requests
 import zipfile
 
 # URL del archivo ZIP que deseas descargar
-url_archivo_zip = "https://edge.forgecdn.net/files/4497/274/server-1.2.3.zip"
+url_archivo_zip = "https://download1582.mediafire.com/wp6wr0d8urqgLWOJRTjpmA2Ct-wu5Ga40m9qnEMP4G14G_ea26O4SNOnuKSq8lhk9Q-ha8dTPhOASYfZ6zSvcE3LmYJqVYP6JBFlLUu9CLwcrE7n7Ry7MU5Qh_PJOtsqy3Qsplm6qCx6c4xdG0B3wkz3H_pvODpz_L36BGXkCA/i188v240ecpa1zf/Minecraft-server-20240513T232640Z-001.zip"
 
 # Ruta de destino para guardar el archivo ZIP
-ruta_destino = "/workspaces/My-server/servidor_minecraft/server-1.2.3.zip"
+ruta_destino = "/workspaces/My-server/Minecraft-server-20240513T232640Z-001.zip"
 
 # Descargar el archivo ZIP
 response = requests.get(url_archivo_zip)
@@ -15,6 +15,6 @@ with open(ruta_destino, "wb") as archivo_zip:
 
 # Descomprimir el archivo ZIP en la misma ubicación
 with zipfile.ZipFile(ruta_destino, "r") as zip_ref:
-    zip_ref.extractall("/workspaces/My-server/servidor_minecraft")
+    zip_ref.extractall("/workspaces/My-server")
 
 print("Archivo ZIP descargado y descomprimido con éxito.")
